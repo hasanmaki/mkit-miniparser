f6273670e64ac5ce3c2966a00138eb4588749f31
hasanmaki <182825954+hasanmaki@users.noreply.github.com>
2025-09-26 17:51:26 +0700

    feat(api): :rocket: implementeing basic api strcture , lifespan and basic cofiguration

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from mkit-miniparser!")
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    """Just welcome message."""
+    return {"message": "Hello World"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app="src.main:app", host="0.0.0.0", port=8000, reload=True)
